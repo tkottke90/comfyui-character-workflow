c4df84645af45b3dad92f36a876d7dd54120b8b5
Thomas Kottke <t.kottke90@gmail.com>
2026-08-21 21:03:37 -0500

    refactor: add base endpoint

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,12 +1,22 @@
 import {} from '@tkottke/hateos-url-manager';
 import { Application } from '../types/application';
 import { Router } from 'express';
-
+import { version } from '../../package.json'
 
 const v1 = Router();
 
 v1.get('/healthcheck', (_, res) => {
   res.send({ status: 'OKAY' })
+});
+
+v1.get('/', (req, res) => {
+
+  return {
+    version,
+    links: {
+      'heathcheck': '/api/v1/healthcheck'
+    }
+  }
 });
 
 export function createAPI(app: Application) {
