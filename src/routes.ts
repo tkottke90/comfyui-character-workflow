687082841b0b5c4bcfc0b384fa8ae3928f2f3ab8
Thomas Kottke <t.kottke90@gmail.com>
2026-08-21 19:31:08 -0500

    refactor: add core structure docs

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,11 @@
+/*
+
+This file outlines the API routes using the HATEOS URL Manager
+library.  This library helps with the construction of links
+in JSON API Responses.  
+
+*/
+
 import { Route } from '@tkottke/hateos-url-manager';
 
 const v1API = new Route('/api/v1');
